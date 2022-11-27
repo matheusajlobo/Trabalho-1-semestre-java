841333e5f781ceda1df342b729befc1b1e8df35a
Matheus Lobo <matheusajlobo@outlook.com>
2022-11-27 03:59:25 -0300

    Primeiros Passos

---
+++ src/application/Principal.java
@@ -0,0 +1,72 @@
+package application;
+
+import entite.Funcionarios;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        List<Funcionarios> funcionarios = new ArrayList<>();
+        int userChoice;
+        System.out.printf("%n%s%n%s%n%s%n%s%n%s%n%s%n", "MENU#", "1 - CADASTRAR", "2 - LISTAR", "3 - BUSCAR", "4 - EXCLUIR", "5 - SAIR");
+        do {
+            System.out.println();
+            userChoice = input.nextInt();
+            switch (userChoice) {
+                case 1:
+                    System.out.printf("%nDigite o numero da matricula: ");
+                    int matricula = input.nextInt();
+                    input.nextLine();
+                    System.out.printf("Digite o nome: ");
+                    String nome = input.nextLine();
+                    System.out.printf("Digite o cargo: ");
+                    String cargo = input.nextLine();
+                    System.out.printf("Digite o setor: ");
+                    String setor = input.nextLine();
+                    System.out.printf("Digite a remuneração: ");
+                    double remuneracao = input.nextDouble();
+                    funcionarios.add(new Funcionarios(matricula, nome, cargo, setor, remuneracao));
+                    break;
+                case 2:/* imprime elementos da lista em formato tabelado */
+                    if (funcionarios != null) {
+                        System.out.format("%n%-20s%-20s%-20s%n","MATRICULA", "NOME", "CARGO");
+                        for (Funcionarios list : funcionarios) {
+                            System.out.format("%-20s%-20s%-20s%n", list.getMatricula(), list.getNome(), list.getCargo());
+                        }
+                    } else {
+                        System.out.println("NENHUM FUNCIONARIO CADASTRADO!!");
+                    }
+                    break;
+                case 3:/*realiza uma busca na lista atravez de uma expressão lambda*/
+                    if (funcionarios != null) {
+                        System.out.println("Digite o numero da matricula: ");
+                        int matBusca = input.nextInt();
+                        /* converte a list pra stream, realiza uma operação lambida e retorna para list*/
+                        List<Funcionarios> resultado = funcionarios.stream().filter(x -> x.getMatricula() == matBusca).collect(Collectors.toList());
+                        System.out.printf("%s",resultado);
+                    } else
+                        System.out.println("NENHUM FUNCIONARIO CADASTRADO!!");
+                    break;
+                case 4:/* realiza uma busca e exclui o item correspondente*/
+                    if (funcionarios != null) {
+                        System.out.println("Digite o numero da matricula: ");
+                        int matBusca = input.nextInt();
+                        funcionarios.removeIf(x -> x.getMatricula() == matBusca);
+                    } else
+                        System.out.println("NENHUM FUNCIONARIO CADASTRADO!!");
+
+                default:
+                    break;
+
+            }
+            System.out.format("%n%-10s%n%-10s%n%-10s%n%-10s%n%-10s%n%-10s%n", "MENU#", "1 - CADASTRAR", "2 - LISTAR", "3 - BUSCAR", "4 - EXCLUIR", "5 - SAIR");
+        } while (userChoice != 5);
+
+
+        input.close();
+    }
+}
---
+++ src/entite/Funcionarios.java
@@ -0,0 +1,73 @@
+package entite;
+
+public class Funcionarios {
+    private String nome;
+    private Integer matricula;
+    private String cargo;
+    private Double remuneracao;
+    private String setor;
+
+    public Funcionarios() {
+    }
+    public Funcionarios(Integer matricula, String nome, String cargo, String setor, Double remuneracao) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.cargo = cargo;
+        this.remuneracao = remuneracao;
+        this.setor = setor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(Integer matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public Double getRemuneracao() {
+        return remuneracao;
+    }
+
+    public void setRemuneracao(Double remuneracao) {
+        this.remuneracao = remuneracao;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
+    @Override
+    public String toString() {
+        return "Matrícula: "
+              + String.format("%d, ",getMatricula())
+              + "Nome: "
+              + getNome()
+              + ", Cargo: "
+              + getCargo()
+              + ", Setor: "
+              + getSetor()
+              + ", Remuneração: "
+              + String.format("%.2f", getRemuneracao())  ;
+    }
+}
